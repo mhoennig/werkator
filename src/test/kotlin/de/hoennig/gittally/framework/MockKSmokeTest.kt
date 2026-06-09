a030f7abb6f7456ff16442cff0a3226399e857bb
Michael Hoennig <michael@hoennig.de>
2026-06-09 15:20:21 +0200

    add ADRs for gradle and spring boot version and version smoke-tests

---
+++ src/test/kotlin/de/hoennig/gittally/framework/MockKSmokeTest.kt
@@ -0,0 +1,23 @@
+package de.hoennig.gittally.framework
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+interface Greeter {
+    fun greet(name: String): String
+}
+
+class MockKSmokeTest : FunSpec({
+
+    test("mockk stubs and verifies") {
+        val greeter = mockk<Greeter>()
+        every { greeter.greet("world") } returns "hello, world"
+
+        greeter.greet("world") shouldBe "hello, world"
+
+        verify(exactly = 1) { greeter.greet("world") }
+    }
+})
---
+++ src/test/kotlin/de/hoennig/gittally/framework/TestcontainersSmokeTest.kt
@@ -0,0 +1,17 @@
+package de.hoennig.gittally.framework
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
+
+class TestcontainersSmokeTest : FunSpec({
+
+    test("Testcontainers starts a container") {
+        val container = GenericContainer(DockerImageName.parse("alpine:3"))
+            .withCommand("sh", "-c", "sleep 30")
+        container.start()
+        container.isRunning shouldBe true
+        container.stop()
+    }
+})
